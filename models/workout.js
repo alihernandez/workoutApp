04d902dde366a75a186355551056ad8cc3c246ac
phantomramen <ali.hernandez9two@gmail.com>
2020-10-25 15:23:40 -0600

    UnhandledPromiseRejectionWarning

--- models/workout.js
+++ models/workout.js
@@ -1,17 +1,51 @@
-const mongoose = require ("mongoose");
-
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 const workoutSchema = new Schema({
-    //defenition of property of model
-        day :{
-            type: Date, 
-            default: new Date().setDate(new Date().getDate())
-        },
-        exercises : {
-            type: Array 
-        }
+  day: {
+    type: Date,
+    default: () => new Date(),
+  },
+  exercises: [
+    {
+      type: {
+        type: String,
+        required: true,
+      },
+      name: {
+        type: String,
+        required: true,
+        trim: true,
+      },
+      duration: {
+        type: Number,
+        required: true,
+      },
+      weight: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
+      distance: {
+        type: Number,
+      },
+    },
+    {
+      toJSON: {
+        // include any virtual properties when data is requested
+        virtuals: true,
+      },
+    },
+  ],
 });
-
-var Workout = mongoose.model("Workout",workoutSchema)
-
-module.exports = Workout
+const Workout = mongoose.model("Workout", workoutSchema);
+workoutSchema.virtual("totalDuration").get(function () {
+  // "reduce" array of exercises down to just the sum of their durations
+  return this.exercises.reduce((total, exercise) => {
+    return total + exercise.duration;
+  }, 0);
+});
+module.exports = Workout;
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,56 +4,52 @@ const router = require("express").Router()
 const db = require("../models")
 
 router.get("/api/workouts", function (req, res){
-    db.Workout.find({}).then(function (dbWorkout){
-        console.log(dbWorkout),
+    db.Workout.find().then(function (dbWorkout){
+        // console.log(dbWorkout),
 res.json(dbWorkout)
     })
 })
 
-// router.post("/api/workouts", function (req, res){
-//     // console.log(req.body);
-//     try{
-//         const response = db.Workout.create({type: "workout"})
-//         res.json(response);
-//     }
-//     catch(err){
-//         console.log("error occurred creating a workout: ", err)
-//     }
-// })
+//create new workout
+router.post('/api/workouts', (req,res) => {
+    db.Workout.create({}).then(newWorkout => {
+        res.json(newWorkout);
+    })
+})
 
 
-// router.put("/api/workouts/:id", ({body, params}, res) => {
-//     // console.log(body, params)
-//     const workoutId = params.id
-//     let savedExercises = []
+router.put("/api/workouts/:id", ({body, params}, res) => {
+    // console.log(body, params)
+    let workoutId = params.id
+    let savedExercises = []
 
 
-// db.Workout.find({_id: workoutId})
-//             .then(dbWorkout => {
-//                 console.log(dbWorkout)
-//                 savedExercises = dbWorkout[0].exercises;
-//                 res.json(dbWorkout[0].exercises);
-//                 let allExercises = [...savedExercises, body]
-//                 console.log(allExercises)
-//                 updateWorkout(allExercises)
-//             })
-//             .catch(err => {
-//                 res.json(err);
-//             })
-//             });
+db.Workout.find({_id: workoutId})
+            .then(dbWorkout => {
+                console.log(dbWorkout)
+                savedExercises = dbWorkout[0].exercises;
+                res.json(dbWorkout[0].exercises);
+                let allExercises = [...savedExercises, body]
+                console.log(allExercises)
+                updateWorkout(allExercises)
+            })
+            .catch(err => {
+                res.json(err);
+            })
+        })
 
-//             function updateWorkout(exercises){
-//                 db.Workout.findByIdAndUpdate(workoutId, {exercises: exercises}, function(err, res){
-//                 if(err){
-//                     console.log(err)
-//                 }
+            function updateWorkout(exercises){
+                db.Workout.findByIdAndUpdate(workoutId, {exercises: exercises}, function(err, res){
+                if(err){
+                    console.log(err)
+                }
     
-//                 })
-//             }
+                })
+            }
 
 router.get("/api/range", function (req, res){
     db.Workout.find().then(function (dbWorkout){
 res.json(dbWorkout)
     })
 })
-module.exports = router;
+module.exports = router
